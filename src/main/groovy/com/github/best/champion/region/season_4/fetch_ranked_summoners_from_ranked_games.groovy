72d2b9f061cd9bd37842fa06ff12b4ce78f85534
Ronald Chen <Ronald.Chen@elasticpath.com>
2014-01-18 23:04:32 -0800

    added extraction of summoners from ranked games

---
+++ src/main/groovy/com/github/best/champion/region/season_4/fetch_ranked_summoners_from_ranked_games.groovy
@@ -0,0 +1,73 @@
+package com.github.best.champion.region.season_4
+
+import com.github.concept.not.found.mongo.groovy.util.MongoUtils
+import com.mongodb.BasicDBObject
+
+MongoUtils.connect {
+	mongo ->
+		mongo.season_4.ranked_games.ensureIndex([
+				"summoners-extracted": 1
+		] as BasicDBObject)
+
+		def done = 0
+		def total = mongo.season_4.ranked_games.count([
+				"summoners-extracted": [
+						'$exists': false
+				]
+		] as BasicDBObject)
+		def start = System.currentTimeMillis()
+		mongo.season_4.ranked_games.find([
+				"summoners-extracted": [
+						'$exists': false
+				]
+		] as BasicDBObject, [
+				"summonerId": 1,
+				"fellowPlayers.summonerId": 1
+		] as BasicDBObject).each {
+			game ->
+				def previousPercentage = 100 * done / total as int
+				upsertSummoner(game.summonerId, mongo.season_4.ranked_summoners)
+				game.fellowPlayers.each {
+					fellowPlayer ->
+						def summonerId = fellowPlayer.summonerId
+						upsertSummoner(summonerId, mongo.season_4.ranked_summoners)
+				}
+
+				mongo.season_4.ranked_games.update(
+						[
+								_id: it._id
+						] as BasicDBObject,
+						[
+								'$set': [
+										"summoners-extracted": true
+								]
+						] as BasicDBObject,
+						true,
+						false
+				)
+
+				done++
+				def currentPercentage = 100 * done / total as int
+				if (previousPercentage != currentPercentage && currentPercentage % 10 == 0) {
+					def timeRemaining = (System.currentTimeMillis() - start) * (total - done) / done as int
+					def hours = timeRemaining / (1000 * 60 * 60) as int
+					def minutes = (timeRemaining / (1000 * 60) as int) % 60
+					def seconds = (timeRemaining / 1000 as int) % 60
+					def duration = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+					println("done $currentPercentage% $done/$total - remaining $duration")
+				}
+		}
+}
+
+def upsertSummoner(summonerId, ranked_summoners) {
+	ranked_summoners.update(
+			[
+					_id: summonerId
+			] as BasicDBObject,
+			[
+					'$set': [:]
+			] as BasicDBObject,
+			true,
+			false
+	)
+}
